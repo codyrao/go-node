e6675b1ce3d1da433aea8d95402919e3c6def6a1
raowangqi <raowangqi@xunlei.com>
2026-02-03 11:43:16 +0800

    [fea] 支持 electron

--- check-electron.js
+++
@@ -1,4 +0,0 @@
-console.log('Electron:', process.versions.electron);
-console.log('Node:', process.versions.node);
-console.log('V8:', process.versions.v8);
-console.log('ABI:', process.versions.modules);
